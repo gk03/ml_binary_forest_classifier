29e754e28b4acdc4020a6f7b5ba25e1cb80c7081
root <root@final.project.gokul>
2016-05-14 23:55:58 -0400

    5

--- emr/edit_ds.py
+++ emr/edit_ds.py
@@ -3,9 +3,9 @@ df = pd.read_csv("minified.csv", sep=',')
 for index, row in df.iterrows():
      #df[row]['PrimaryDiagnosisCode'] = df[row]['PrimaryDiagnosisCode'].split(".",1)[0]
      #print 'index',index,'row',row
-     df.loc[index,'PrimaryDiagnosisCode']  = row['PrimaryDiagnosisCode'].split(".",1)[0] 
+     df.loc[index,'PrimaryDiagnosisCode']  = row['PrimaryDiagnosisCode'][0] 
      #row = row.split(".",1)[0]	
 	 #print row[i]	        
 
-df.to_csv('minified_1.csv') 
+df.to_csv('minified_2.csv') 
 
